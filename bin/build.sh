3b703ed5254b8db113a7514cf7cf5be07c262e8a
jacketchen <342115615@qq.com>
2021-09-13 10:32:10 +0800

    fix: build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-dir="$(dirname "${BASH_SOURCE[0]}")/.."
-
-rm -rf $dir/dist \
+rm -rf dist \
   && npx tsc \
-  && npx uglifyjs --compress --mangle --output $dir/dist/index.js $dir/dist/index.js \
-  && npx uglifyjs --compress --mangle --output $dir/dist/utils/index.js $dir/dist/utils/index.js \
-  && npx uglifyjs --compress --mangle --output $dir/dist/constants/index.js $dir/dist/constants/index.js
+  && npx uglifyjs --compress --mangle --output dist/index.js dist/index.js \
+  && npx uglifyjs --compress --mangle --output dist/utils/index.js dist/utils/index.js \
+  && npx uglifyjs --compress --mangle --output dist/constants/index.js dist/constants/index.js
